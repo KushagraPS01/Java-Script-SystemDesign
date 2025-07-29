5638ec6f9b40b7a4c8041a88d5655993e34474ca
KushagraPS01 <kushagrapratap01@gmail.com>
2025-07-29 20:00:55 +0530

    Upload Files

--- taskscheduler.js
+++ taskscheduler.js
@@ -4,6 +4,15 @@ class TaskScheduler {
         this.runningTasks = 0;
     }
 
+    getNextTask() {
+        if (this.runningTasks < this.concurrency &&
+            this.__waitingQueue.length > 0
+        ){
+            const NextTask = this.__waitingQueue.shift();
+            NextTask();
+        }
+    }
+
     addTask() {
         return new Promise((resolve, reject)  => {
             async function __taskRunner() {
@@ -19,17 +28,28 @@ class TaskScheduler {
                 }
                 finally {
                     this.runningTasks -= 1;     // Task completed 
+                    this.getNextTask();         // Check task in Queue
                 }
             }
             if (this.runningTasks < this.concurrency) {
-                __taskRunner();
+                __taskRunner.call(this);
             }
             else {
-                this.__waitingQueue.push(__taskRunner);
+                this.__waitingQueue.push(__taskRunner.bind(this));
             }
         })
     }
 
 }
 
+const scheduler = new TaskScheduler(2);
 
+scheduler.addTask(
+    () => new Promise((res) => setTimeout(() => ('Task1'), 1000))
+);
+scheduler.addTask(
+    () => new Promise((res) => setTimeout(() => ('Task2'), 1000))
+);
+scheduler.addTask(
+    () => new Promise((res) => setTimeout(() => ('Task3'), 1000))
+);
